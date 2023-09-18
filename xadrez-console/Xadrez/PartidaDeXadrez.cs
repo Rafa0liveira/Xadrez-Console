4064caca15293b1a491a5ecd5004580b4a88afc8
Rafael Oliveira <orafael767@gmail.com>
2023-09-18 21:05:33 +0100

    Jogada especial Promocao

--- xadrez-console/Xadrez/PartidaDeXadrez.cs
+++ xadrez-console/Xadrez/PartidaDeXadrez.cs
@@ -128,6 +128,21 @@ namespace Xadrez {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque");
             }
+            Peca p = tab.peca(destino);
+            //JogadaEspecial Promoção
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) ||
+                (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -146,7 +161,7 @@ namespace Xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
             //JogadaEspecial En Passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
             {
